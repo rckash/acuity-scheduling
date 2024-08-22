71da7e7b9267dadf0aafeee94ce9de3c5282665e
RC <rckashyapcpe@gmail.com>
2024-08-22 11:23:27 +0800

    Update slider component

--- scripts/components/slider.js
+++ scripts/components/slider.js
@@ -2,10 +2,12 @@ let scrollContainer = document.getElementsByClassName("slider")[0];
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
 
-backBtn.addEventListener("click", () => {
+backBtn.addEventListener("click", (evt) => {
     scrollContainer.scrollLeft -= 350;
+    console.log("backBtn clicked!");
 });
 
 nextBtn.addEventListener("click", () => {
     scrollContainer.scrollLeft += 350;
+    console.log("nextBtn clicked!");
 });
